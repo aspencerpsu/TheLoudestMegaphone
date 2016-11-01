2230b601a21bee89f8cd64c57da350630b779f0d
Akeem Leighton Foster Spencer <akeem@schwartzenegger.aint.on.no.joke.sh1t>
2016-11-01 21:20:24 +0000

    states resolved

---
+++ Casualties/Casualties/spiders/today_victim.py
@@ -0,0 +1,42 @@
+import scrapy
+from time import strftime, strptime, localtime, mktime, time as currentTime
+
+class NewYorkSpider(scrapy.Spider):
+	
+	name = "fatalities"
+	# A query we will use for later when there is more reliable results
+	# str(list_of_columns[0].css("::text").extract_first()) != strftime("%B %d, %Y", gmtime())
+	# if str(list_of_columns[1].css("::text").extract_first()) != "Texas" 
+
+	start_urls = ["http://www.gunviolencearchive.org/last-72-hours", ]
+	
+	def parse(self, response):
+		four_days_ago = currentTime() - 345600
+
+
+		for row in response.css("table.responsive tbody tr"):
+			list_of_columns = row.css("td")
+			cell_date = list_of_columns[0].css("::text").extract_first()
+			epoch_date = strptime(cell_date, "%B %d, %Y")
+					
+			if int(list_of_columns[5].css("::text").extract_first()) == 0 or \
+			mktime(epoch_date) <= four_days_ago:
+				pass
+			
+			else:
+				yield {
+					"losses": list_of_columns[5].css("::text").extract_first(),
+					"incident": list_of_columns[6].css(" ul li a::attr('href')").extract_first(),
+					"source": list_of_columns[6].css("ul li.last a::attr('href')").extract_first(),
+					"date": list_of_columns[0].css("::text").extract_first(),
+					"state": list_of_columns[1].css("::text").extract_first(),
+				}
+
+			
+			#Create a field that checks the to see if there's another page with records 
+			# greater than 4 days ago
+
+		next_page = response.css('li.pager-next a::attr(href)').extract_first()
+		if next_page is not None:
+			next_page = response.urljoin(next_page)
+			yield scrapy.Request(next_page, callback=self.parse) 	
---
+++ tweets/views.py
@@ -0,0 +1,85 @@
+from django.shortcuts import render
+import sys, os, shutil, tweepy, collections
+from tweepy import API
+import json
+from io import StringIO
+
+
+# Create your views here.
+
+auth = tweepy.OAuthHandler("Axt0mv2kupxboLs30wSlWy73s", "JCBmN0ykBiHr7XFSVFJI1JGrfwtXlWnGNFA56tXIdx1aXXxAET")
+
+auth.set_access_token("781216035936538625-tsdTVa15PPxts0bWcJaABAm43HewBCg", "	9kkCjBXXhROi1gXvxWxZgHlhn1iMPFg0DZI7Jj2ZApq76")
+
+api = tweepy.API(auth)
+
+def convert(data):
+    if isinstance(data, basestring):
+	return str(data)
+    elif isinstance(data, collections.Mapping):
+	return dict(map(convert, data.iteritems()))
+    elif isinstance(data, collections.Iterable):
+	return type(data)(map(convert, data))
+    else:
+	return data
+
+def tweets_index(request):
+	if os.path.isfile('/home/akeem/NewYorkFatalities/Casualties/affected.json'):
+		shutil.move('/home/akeem/NewYorkFatalities/Casualties/affected.json', "/home/akeem/NewYorkFatalities/affected.json")
+		json_file = open(r'affected.json').read()
+		converted_json_file = StringIO(unicode(json_file))
+		json_dictionary = json.load(converted_json_file)
+	
+	else:
+		json_file = open(r'affected.json').read()
+                converted_json_file = StringIO(unicode(json_file))
+                json_dictionary = json.load(converted_json_file)
+
+	mod_json_dictionary = [convert(json_dictionary[x].items()) for x in range(0, len(json_dictionary))]
+	dates = [mod_json_dictionary[x][0][1] for x in range(0, len(mod_json_dictionary))]
+
+		
+	killed=[int(mod_json_dictionary[x][3][1]) for x in range(0, len(mod_json_dictionary))]
+	
+	state = [mod_json_dictionary[x][4][1] for x in range(0,len(mod_json_dictionary))]
+	
+	state_by_state = []
+
+	states = {'name': []}
+
+	iterobject = iter(json_dictionary)
+				
+	for row in json_dictionary:
+		next_item = iterobject.next()
+		if not states['name'].__contains__(next_item['state']):
+			state_by_state.append(next_item)
+			states['name'].append(next_item['state'])
+		else:
+			for index in state_by_state:
+				if index['state'] == next_item['state']:
+					index['losses'] = str(int(index['losses']) + int(next_item['losses']))
+				else:
+					pass 
+						
+					
+	
+	mod_state_by_state = [state_by_state[x].items()) for x in range(0, len(state_by_state))]
+
+	
+	sbs_killed=[int(mod_state_by_state[x][3][1]) for x in range(0, len(mod_state_by_state))]
+	
+	sbs_states = [mod_state_by_state[x][4][1] for x in range(0,len(mod_state_by_state))]
+	context = {
+			"tragedies": mod_json_dictionary,
+			"states": state,
+			"killed": killed,
+			"dates": list(set(dates)),
+			"state_by_state": mod_state_by_state,
+			"total_by_state": sbs_killed,
+			"cum_states": sbs_states
+		}
+	
+	return render(request, "home.html", context)
+	
+	
+	
