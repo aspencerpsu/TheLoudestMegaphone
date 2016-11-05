4fe9b74ba0892b7413ef997171b8c0f294c13722
Akeem Leighton Foster Spencer <akeem@schwartzenegger.aint.on.no.joke.sh1t>
2016-11-04 21:08:26 -0400

    Tweet model added

--- tweets/models.py
+++ tweets/models.py
@@ -1,5 +1,14 @@
 from __future__ import unicode_literals
-
+from django.conf import settings
 from django.db import models
 
+#add the datefield to the models you're creating, also add UIDs
+#that are unique to this model
+from time import strftime, localtime
+
 # Create your models here.
+
+class Tweet(models.Model):
+	uid=models.IntegerField(unique=True)
+	source=models.TextField(max_length=None)
+	date=models.DateField(null=False, default=strftime("%Y-%m-%d", localtime()))
