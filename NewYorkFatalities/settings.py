2c9a82e0fac61d03a29a8e8f6baea670a3446e62
Arun Mavumkal <arunmavumkal@Aruns-MBP.fios-router.home>
2016-11-12 00:07:11 -0500

    modified settings.py

--- NewYorkFatalities/settings.py
+++ NewYorkFatalities/settings.py
@@ -97,8 +97,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'loudestmegaphone',
-	'USER': 'loudestone',
-	'PASSWORD': 'changetheworld',
+	'USER': os.getenv('DB_USER'),
+	'PASSWORD': os.getenv('DB_PASS'),
 	'HOST': 'localhost',
 	'POST': '',
 	'BROKER_URL': 'django://'
