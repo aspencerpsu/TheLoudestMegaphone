17d60893a7c5855a5169e35435a74c221e289e88
Akeem Leighton Foster Spencer <akeem@schwartzenegger.aint.on.no.joke.sh1t>
2016-11-21 17:32:40 -0500

    added aggregate function from models module

--- tweets/models.py
+++ tweets/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 from django.conf import settings
 from django.db import models
+from django.db.models import Sum
 
 #add the datefield to the models you're creating, also add UIDs
 #that are unique to this model
@@ -12,3 +13,7 @@ class Tweet(models.Model):
 	uid=models.IntegerField(unique=True)
 	source=models.TextField(max_length=None)
 	date=models.DateField(null=False, default=strftime("%Y-%m-%d", localtime()))
+        killed=models.IntegerField()
+	state=models.CharField(max_length=20, default=None)
+
+
