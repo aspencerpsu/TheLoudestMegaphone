b8b1bd179851d139ac0f60b7b6076a4ee10a0a7c
Akeem Leighton Foster Spencer <akeem@schwartzenegger.aint.on.no.joke.sh1t>
2016-11-21 17:32:07 -0500

    extra models appended

--- GunViolenceArchive/models.py
+++ GunViolenceArchive/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+import uuid
 
 class GunVioleceArchive(models.Model):
     date = models.DateField(auto_now=False, auto_now_add=False)
@@ -8,12 +9,25 @@ class GunVioleceArchive(models.Model):
     city = models.CharField(max_length=120)
     state = models.CharField(max_length=10)
     is_police_involved = models.BooleanField(default=False)
-    shot_dead_by_police = models.BooleanField(default=False)
-    civilian_injury = models.SmallIntegerField(default=0)
-    civilian_death = models.SmallIntegerField(default=0)
-    police_injury = models.SmallIntegerField(default=0)
-    police_death = models.SmallIntegerField(default=0)
 
     def __unicode__ (self):
         return self.id
 
+class Incident(models.Model):
+	uid=models.UUIDField(primary_key=True,default=uuid.uuid4,unique=True)
+	date = models.DateField(auto_now=False, auto_now_add=False)
+	address = models.CharField(max_length=120, default=None, blank=True, null=True)
+	city = models.CharField(max_length=120)
+	state = models.CharField(max_length=2)
+	source = models.CharField(max_length=4096)
+	
+	
+class Victim(models.Model):
+    incident=models.ForeignKey(Incident,on_delete=models.CASCADE)	
+    suicide=models.BooleanField(default=False)
+    shot_dead_by_police = models.BooleanField(default=False)
+    civilian_injury = models.BooleanField(default=False)
+    civilian_death = models.BooleanField(default=False)
+    police_injury = models.BooleanField(default=False)
+    police_death = models.BooleanField(default=False)
+    is_police = models.BooleanField(default=False)
